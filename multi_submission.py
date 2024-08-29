8ad75a6ae2f55d0579af661ff83ee8e08f5ffca1
jaimecomprendre <maxime.hubert@student-cs.fr>
2024-08-29 16:32:33 +0200

    New multi submission scripts

--- multi_submission.py
+++ multi_submission.py
@@ -1,25 +1,36 @@
-# Copyright 2024 tu-studio
-# This file is licensed under the Apache License, Version 2.0.
-# See the LICENSE file in the root of this project for details.
-
 import itertools
 import subprocess
 import os 
 
-# Submit experiment for hyperparameter combination
-def submit_batch_job(test_split, batch_size):
-    # Set dynamic parameters for the batch job as environment variables
-    # But dont forget to add the os.environ to the new environment variables otherwise the PATH is not found
+def calculate_hyperparams(n_bands):
+    # Base values for n_bands = 1
+    base_n_blocks = 4
+    base_kernel_size = 13
+    base_dilation_growth = 10
+    
+    # Scale hyperparameters based on n_bands
+    n_blocks = max(2, base_n_blocks - (n_bands - 1) // 2)  # Decrease n_blocks as n_bands increases
+    kernel_size = max(3, base_kernel_size - (n_bands - 1))  # Decrease kernel_size as n_bands increases
+    dilation_growth = max(2, base_dilation_growth - (n_bands - 1))  # Decrease dilation_growth as n_bands increases
+    
+    return n_blocks, kernel_size, dilation_growth
+
+def submit_batch_job(n_bands, use_spectral_loss):
+    n_blocks, kernel_size, dilation_growth = calculate_hyperparams(n_bands)
+    
     env = {
         **os.environ,
-        "EXP_PARAMS": f"-S preprocess.test_split={test_split} -S train.batch_size={batch_size}"
+        "EXP_PARAMS": (f"-S train.n_bands={n_bands} "
+                       f"-S train.n_blocks={n_blocks} "
+                       f"-S train.kernel_size={kernel_size} "
+                       f"-S train.dilation_growth={dilation_growth} "
+                       f"-S train.use_spectral_loss={str(use_spectral_loss).lower()}")
     }
-    # Run sbatch command with the environment variables as bash! subprocess! command (otherwise module not found)
     subprocess.run(['/usr/bin/bash', '-c', 'sbatch slurm_job.sh'], env=env)
 
 if __name__ == "__main__":
-    test_split_list = [0.2, 0.3]
-    batch_size_list = [2048, 4096]
-    # Iterate over a cartesian product parameter grid of the test_split and batch_size lists
-    for test_split, batch_size in itertools.product(test_split_list, batch_size_list):
-        submit_batch_job(test_split,batch_size)
+    n_bands_list = [1, 4, 8, 16]
+    use_spectral_loss_list = [True, False]
+    
+    for n_bands, use_spectral_loss in itertools.product(n_bands_list, use_spectral_loss_list):
+        submit_batch_job(n_bands, use_spectral_loss)
