9e42617f6d5a098150c544ebae2a5f2ca4ccf44f
jaimecomprendre <maxime.hubert@student-cs.fr>
2024-09-16 17:51:50 +0200

    New multi submission script

--- multi_submission.py
+++ multi_submission.py
@@ -36,24 +36,28 @@ def find_optimal_params(input_size, n_bands, n_blocks):
     return max_kernel_size, max_dilation_growth
 
 def generate_hyperparams():
-    n_bands_options = [1, 2, 4, 8, 16]
-    n_blocks_options = range(2, 7)  # 2 to 6
+    configurations = [
+        (2, 17, 3, 15),
+        (1, 13, 4, 10),
+        (16, 3, 6, 4)
+    ]
     
     use_kl_options = [False]
     use_adversarial_options = [False]
     use_skips_options = [True]
-    use_noise_options = [True]
+    use_noise_options = [False]
+    use_residual_stack_options = [True, False]
+    use_wn_options = [True, False]
+    use_batch_norm_options = [True, False]
 
-    input_size = 508032  # From params.yaml
-
-    for n_bands, n_blocks, use_kl, use_adversarial, use_skips, use_noise in itertools.product(
-        n_bands_options, n_blocks_options,
-        use_kl_options, use_adversarial_options, use_skips_options, use_noise_options
+    for (n_bands, kernel_size, n_blocks, dilation_growth), use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm in itertools.product(
+        configurations,
+        use_kl_options, use_adversarial_options, use_skips_options, use_noise_options,
+        use_residual_stack_options, use_wn_options, use_batch_norm_options
     ):
-        kernel_size, dilation_growth = find_optimal_params(input_size, n_bands, n_blocks)
-        yield n_bands, kernel_size, n_blocks, dilation_growth, use_kl, use_adversarial, use_skips, use_noise
+        yield n_bands, kernel_size, n_blocks, dilation_growth, use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm
 
-def submit_batch_job(n_bands, kernel_size, n_blocks, dilation_growth, use_kl, use_adversarial, use_skips, use_noise):
+def submit_batch_job(n_bands, kernel_size, n_blocks, dilation_growth, use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm):
     env = {
         **os.environ,
         "EXP_PARAMS": (f"-S train.n_bands={n_bands} "
@@ -63,11 +67,15 @@ def submit_batch_job(n_bands, kernel_size, n_blocks, dilation_growth, use_kl, us
                        f"-S train.use_kl={str(use_kl).lower()} "
                        f"-S train.use_adversarial={str(use_adversarial).lower()} "
                        f"-S train.use_noise={str(use_noise).lower()} "
-                       f"-S train.use_skip={str(use_skips).lower()}")
+                       f"-S train.use_skip={str(use_skips).lower()} "
+                       f"-S train.use_residual_stack={str(use_residual_stack).lower()} "
+                       f"-S train.use_wn={str(use_wn).lower()} "
+                       f"-S train.use_batch_norm={str(use_batch_norm).lower()}")
     }
     subprocess.run(['/usr/bin/bash', '-c', 'sbatch slurm_job.sh'], env=env)
     print(f"Submitted job: n_bands={n_bands}, n_blocks={n_blocks}, kernel_size={kernel_size}, dilation_growth={dilation_growth}, "
-          f"use_kl={use_kl}, use_adversarial={use_adversarial}, use_skips={use_skips}, use_noise={use_noise}")
+          f"use_kl={use_kl}, use_adversarial={use_adversarial}, use_skips={use_skips}, use_noise={use_noise}, "
+          f"use_residual_stack={use_residual_stack}, use_wn={use_wn}, use_batch_norm={use_batch_norm}")
 
 if __name__ == "__main__":
     total_configurations = 0
