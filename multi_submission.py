afb69eb98ea3c3c732f67f02010e68f9cbf3347e
jaimecomprendre <maxime.hubert@student-cs.fr>
2024-09-18 17:39:46 +0200

    New multi submission scripts

--- multi_submission.py
+++ multi_submission.py
@@ -3,22 +3,35 @@ import subprocess
 import os
 import math
 
-def calculate_final_input_size(input_size, n_bands, dilation_growth, n_blocks, kernel_size):
+    
+def calculate_compression(input_length, n_bands, n_blocks, kernel_size, dilation_growth, stride=2):
+    output_length = input_length
     if n_bands > 1:
-        padded_input_size = 2 ** math.ceil(math.log2(input_size))
+        padded_input_size = 2 ** math.ceil(math.log2(input_length))
     else:
-        padded_input_size = input_size
+        padded_input_size = input_length
+
+    output_length = padded_input_size // n_bands
     
-    pqmf_size = padded_input_size // n_bands
+    for _ in range(n_blocks):
+        dilation = dilation_growth ** ( _ + 1 ) 
+        # padding = (kernel_size - 1) * dilation  // 2
+        padding = 0
+        output_length = (output_length + 2 * padding - dilation * (kernel_size - 1) - 1) // stride + 1
+
+    # Final convolutional layer (conv_latent)
+    final_kernel_size = 5
+    final_padding = 2
+    final_stride = 1
+    output_length = ((output_length + 2 * final_padding - (final_kernel_size - 1) - 1) // final_stride) + 1
+
+    compression_rate = input_length / output_length
     
-    rf = 0
-    for i in range(n_blocks):
-        dilation = dilation_growth ** (i + 1)
-        rf += (kernel_size - 1) * dilation
-    
-    final_size = pqmf_size - rf
-    
-    return final_size
+    return {
+        "input_length": input_length,
+        "output_length": output_length,
+        "compression_rate": compression_rate
+    }
 
 def find_optimal_params(input_size, n_bands, n_blocks):
     max_kernel_size = 3
@@ -26,8 +39,8 @@ def find_optimal_params(input_size, n_bands, n_blocks):
     
     for kernel_size in range(3, 18):
         for dilation_growth in range(1, 16):
-            final_size = calculate_final_input_size(input_size, n_bands, dilation_growth, n_blocks, kernel_size)
-            if final_size > 0:
+            result = calculate_compression(input_size, n_bands, n_blocks, kernel_size, dilation_growth)
+            if result['output_length'] > 0:
                 max_kernel_size = kernel_size
                 max_dilation_growth = dilation_growth
             else:
@@ -37,20 +50,20 @@ def find_optimal_params(input_size, n_bands, n_blocks):
 
 def generate_hyperparams():
     configurations = [
-        (2, 17, 3, 15),
-        (1, 13, 4, 10),
-        (16, 3, 6, 4)
+        (2, 19, 3, 15)
+        
     ]
-    
+    # (1, 13, 4, 10)
+    # (16, 3, 6, 4)
+
     use_kl_options = [False]
     use_adversarial_options = [False]
     use_skips_options = [True]
     use_noise_options = [False]
-    use_residual_stack_options = [True, False]
-    use_wn_options = [True, False]
-    use_batch_norm_options = [True, False]
+    use_residual_stack_options = [True]
+    use_wn_options = [True]
+    use_batch_norm_options = [True]
     loss_function_options = ['combined', 'mse', 'spectral_distance']
-
     for (n_bands, kernel_size, n_blocks, dilation_growth), use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm, loss_function in itertools.product(
         configurations,
         use_kl_options, use_adversarial_options, use_skips_options, use_noise_options,
@@ -76,10 +89,18 @@ def submit_batch_job(n_bands, kernel_size, n_blocks, dilation_growth, use_kl, us
                        f"-S metrics.loss_function={loss_function}")
     }
     subprocess.run(['/usr/bin/bash', '-c', 'sbatch slurm_job.sh'], env=env)
+    
+    # Calculate compression
+    input_size = 508032 
+    compression_result = calculate_compression(input_size, n_bands, n_blocks, kernel_size, dilation_growth)
+    
     print(f"Submitted job: n_bands={n_bands}, n_blocks={n_blocks}, kernel_size={kernel_size}, dilation_growth={dilation_growth}, "
           f"use_kl={use_kl}, use_adversarial={use_adversarial}, use_skips={use_skips}, use_noise={use_noise}, "
           f"use_residual={use_residual_stack}, use_wn={use_wn}, use_batch_norm={use_batch_norm}, "
           f"loss_function={loss_function}")
+    print(f"Compression: input_length={compression_result['input_length']}, "
+          f"output_length={compression_result['output_length']}, "
+          f"compression_rate={compression_result['compression_rate']:.2f}x")
 
 if __name__ == "__main__":
     total_configurations = 0
