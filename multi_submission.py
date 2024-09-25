c1536556a5e1468a72f7848e977255222a3411af
jaimecomprendre <maxime.hubert@student-cs.fr>
2024-09-25 17:01:18 +0200

    New multi submission scripts

--- multi_submission.py
+++ multi_submission.py
@@ -37,29 +37,29 @@ def find_optimal_params(input_size, n_bands, n_blocks):
 
 def generate_hyperparams():
     configurations = [
-        (2, 17, 3, 15),
-        (1, 13, 4, 10),
-        (16, 3, 6, 4)
+        (1, 13, 4, 10)
     ]
     
     use_kl_options = [False]
     use_adversarial_options = [False]
     use_skips_options = [True]
     use_noise_options = [False]
-    use_residual_stack_options = [True, False]
-    use_wn_options = [True, False]
-    use_batch_norm_options = [True, False]
-    loss_function_options = ['combined', 'mse', 'spectral_distance']
+    use_residual_stack_options = [True]
+    use_wn_options = [False]
+    use_batch_norm_options = [False]
+    loss_function_options = ['combined']
+    activation_options = ['prelu', 'leaky_relu']
+    use_latent_options = ['dense', 'conv']
 
-    for (n_bands, kernel_size, n_blocks, dilation_growth), use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm, loss_function in itertools.product(
+    for (n_bands, kernel_size, n_blocks, dilation_growth), use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm, loss_function, activation, use_latent in itertools.product(
         configurations,
         use_kl_options, use_adversarial_options, use_skips_options, use_noise_options,
         use_residual_stack_options, use_wn_options, use_batch_norm_options,
-        loss_function_options
+        loss_function_options, activation_options, use_latent_options
     ):
-        yield n_bands, kernel_size, n_blocks, dilation_growth, use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm, loss_function
+        yield n_bands, kernel_size, n_blocks, dilation_growth, use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm, loss_function, activation, use_latent
 
-def submit_batch_job(n_bands, kernel_size, n_blocks, dilation_growth, use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm, loss_function):
+def submit_batch_job(n_bands, kernel_size, n_blocks, dilation_growth, use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm, loss_function, activation, use_latent):
     env = {
         **os.environ,
         "EXP_PARAMS": (f"-S train.n_bands={n_bands} "
@@ -73,13 +73,15 @@ def submit_batch_job(n_bands, kernel_size, n_blocks, dilation_growth, use_kl, us
                        f"-S train.use_residual={str(use_residual_stack).lower()} "
                        f"-S train.use_wn={str(use_wn).lower()} "
                        f"-S train.use_batch_norm={str(use_batch_norm).lower()} "
-                       f"-S metrics.loss_function={loss_function}")
+                       f"-S metrics.loss_function={loss_function} "
+                       f"-S train.activation={activation} "
+                       f"-S train.use_latent={use_latent}")
     }
     subprocess.run(['/usr/bin/bash', '-c', 'sbatch slurm_job.sh'], env=env)
     print(f"Submitted job: n_bands={n_bands}, n_blocks={n_blocks}, kernel_size={kernel_size}, dilation_growth={dilation_growth}, "
           f"use_kl={use_kl}, use_adversarial={use_adversarial}, use_skips={use_skips}, use_noise={use_noise}, "
           f"use_residual={use_residual_stack}, use_wn={use_wn}, use_batch_norm={use_batch_norm}, "
-          f"loss_function={loss_function}")
+          f"loss_function={loss_function}, activation={activation}, use_latent={use_latent}")
 
 if __name__ == "__main__":
     total_configurations = 0
