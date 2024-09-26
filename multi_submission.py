b0d2088ce8e04933120da8f63174d80e24ae0ce9
jaimecomprendre <maxime.hubert@student-cs.fr>
2024-09-26 16:32:12 +0200

    new Multi submission script

--- multi_submission.py
+++ multi_submission.py
@@ -3,30 +3,44 @@ import subprocess
 import os
 import math
 
-def calculate_final_input_size(input_size, n_bands, dilation_growth, n_blocks, kernel_size):
+def calculate_final_input_size(input_size, n_bands, dilation_growth, n_blocks, kernel_size, dilate_conv, dense=False):
+    output_length = input_size
+    stride=1
     if n_bands > 1:
         padded_input_size = 2 ** math.ceil(math.log2(input_size))
     else:
         padded_input_size = input_size
-    
-    pqmf_size = padded_input_size // n_bands
-    
-    rf = kernel_size - 1
-    for i in range(1, n_blocks + 1):
-        dilation = dilation_growth ** i
-        rf += (kernel_size - 1) * dilation
-    
-    final_size = pqmf_size - rf
-    
-    return final_size
 
-def find_optimal_params(input_size, n_bands, n_blocks):
+    output_length = padded_input_size // n_bands
+    
+    for _ in range(n_blocks):
+        dilation = dilation_growth ** (_ + 1)
+        # padding = (kernel_size - 1) * dilation  // 2
+        padding = 0
+        output_length = (output_length + 2 * padding - dilation * (kernel_size - 1) - 1) // stride + 1
+
+    if not dense:
+        # Final convolutional layer (conv_latent)
+        final_dilation = dilation_growth ** n_blocks
+        final_kernel_size = kernel_size
+        final_padding = 0
+        final_stride = 1
+        if dilate_conv:
+            output_length = ((output_length + 2 * final_padding - final_dilation * (final_kernel_size - 1) - 1) // final_stride) + 1
+        else:
+            output_length = ((output_length + 2 * final_padding - (final_kernel_size - 1) - 1) // final_stride) + 1
+    
+    
+    
+    return output_length
+
+def find_optimal_params(input_size, n_bands, n_blocks, dilate_conv=False, dense=False):
     max_kernel_size = 3
     max_dilation_growth = 1
     
     for dilation_growth in range(1, 16):
         for kernel_size in range(3, 18):
-            final_size = calculate_final_input_size(input_size, n_bands, dilation_growth, n_blocks, kernel_size)
+            final_size = calculate_final_input_size(input_size, n_bands, dilation_growth, n_blocks, kernel_size, dilate_conv, dense)
             if final_size > 0:
                 max_kernel_size = kernel_size
                 max_dilation_growth = dilation_growth
@@ -40,11 +54,12 @@ def generate_hyperparams(input_size):
     n_blocks_options = [4]
     latent_dim_options = [32, 64, 16]
     use_skips_options = [True, False]
+    use_latent_options = ['dense', 'conv']
     
     configurations = []
     
     for n_bands, n_blocks in itertools.product(n_bands_options, n_blocks_options):
-        kernel_size, dilation_growth = find_optimal_params(input_size, n_bands, n_blocks)
+        kernel_size, dilation_growth = find_optimal_params(input_size, n_bands, n_blocks, True, False)
         configurations.append((n_bands, kernel_size, n_blocks, dilation_growth))
     
     use_kl_options = [False]
@@ -55,7 +70,7 @@ def generate_hyperparams(input_size):
     use_batch_norm_options = [False]
     loss_function_options = ['combined']
     activation_options = ['prelu', 'leaky_relu']
-    use_latent_options = ['dense', 'conv']
+    
 
     for (n_bands, kernel_size, n_blocks, dilation_growth), latent_dim, use_kl, use_adversarial, use_skips, use_noise, use_residual_stack, use_wn, use_batch_norm, loss_function, activation, use_latent in itertools.product(
         configurations, latent_dim_options, use_kl_options, use_adversarial_options, use_skips_options, use_noise_options,
